5a82fa2ce9f223b207476dcc9b10b7a7a5187eed
Vivek Anand <65071414+iamvik20@users.noreply.github.com>
2021-09-19 15:47:46 +0545

    Add files via upload

---
+++ src/Components/directory/directory.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import MenuItem from '../menu-item/menu-item';
+import './directory.scss';
+import { createStructuredSelector } from 'reselect';
+import { selectDirectorySections } from '../../redux/directory/directory.selector';
+
+
+const Directory = ({sections}) => {
+        return (
+            <div className='directory-menu'>
+                {
+                    sections.map(({ id, ...sections }) => (
+                        <MenuItem  key={id} {...sections} />
+                    ))
+                }
+            </div>
+        )
+}
+
+
+const mapStateToProps =  createStructuredSelector({
+    sections:  selectDirectorySections
+})
+
+export default connect(mapStateToProps)(Directory);
+
